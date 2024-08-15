3b9ab79f70128884daa23444ff938c7b0364b626
akpax <austin.paxton00@gmail.com>
2024-08-14 19:10:21 -0700

    add data exploration and utils files

--- utils/congress_trades_helpers.py
+++ utils/congress_trades_helpers.py
@@ -52,11 +52,6 @@ def calc_gains(group):
     shares_sold = []
     for i, trade in enumerate(group.iterrows()):
         trade = trade[1]
-        print(f"__________________RECORD: {i} __________________")
-        print(trade)
-
-        # skip first trade if it is a sale
-        # no previous purchase and we assume no short selling
         if trade["Transaction"] == "Purchase":
             total_value += trade["Amount"]
             total_cost += trade["Amount"] * trade["trade_price"]
@@ -64,9 +59,7 @@ def calc_gains(group):
             shares_owned.append(
                 trade["Amount"] / trade["trade_price"] + shares_owned[-1]
             )
-            print(f"{total_value=}")
-            print(f"{total_cost=}")
-            print(f"Cost Basis: {cost_basis}")
+
         if trade["Transaction"] == "Sale" and shares_owned[-1] != 0:
             r_gain = round(100 * (trade["trade_price"] - cost_basis) / cost_basis, 2)
             pending_shares_sold = trade["Amount"] / trade["trade_price"]
@@ -83,8 +76,6 @@ def calc_gains(group):
             shares_sold.append(pending_shares_sold)
             shares_owned.append(pending_shares_owned)
 
-            print(f"{cost_basis=}")
-            print(f"{r_gain=}%")
             percent_gain_realized.append(r_gain)
         if (
             i == len(group) - 1 and shares_owned[-1] != 0
@@ -92,17 +83,13 @@ def calc_gains(group):
             unrealized_percent_gains = round(
                 100 * (trade["current_price"] - cost_basis) / cost_basis, 2
             )
-            print(f"{unrealized_percent_gains=}%")
+
             unrealized_gains_per_year = (
                 unrealized_percent_gains / time_interval_to_normalize_unrealized(group)
             )
 
-        # calculate realized percent gain weighted by number of shares sold
-        print(f"{shares_owned=}")
-    print(f"{shares_sold}")
-
+    # calculate realized percent gain weighted by number of shares sold
     if shares_sold != []:
-
         realized_percent_gain_weighted = round(
             sum(np.array(shares_sold) * np.array(percent_gain_realized))
             / sum(np.array(shares_sold)),
@@ -111,7 +98,6 @@ def calc_gains(group):
         realized_gains_per_year = (
             realized_percent_gain_weighted / time_interval_to_normalize_realized(group)
         )
-        print(f"{realized_percent_gain_weighted=}%")
 
     return (
         realized_percent_gain_weighted,
